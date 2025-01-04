301b3de55b8dc29f3549d846ae332df10f6cb128
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-01-04 22:01:45 +0100

    feat: added main page

--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const db = require("./config/db");
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend/src/public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("../frontend/src/index");
 });
 
 loginRoute = require("./routes/login.js");
